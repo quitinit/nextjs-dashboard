f4cde2b12142b91170a47d0aa7ccaa09439000a5
Michael Dinitz <michaeldinitz@yahoo.de>
2025-04-21 16:11:31 +0200

    fixed fonts error

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import {Inter,Lusitana} from 'next/font/google'
 
-export const lusitana = Lusitana({weight: ["400","700"]})
+export const lusitana = Lusitana({subsets: ['latin'],weight: ["400","700"]})
 export const inter = Inter({subsets: ['latin']});
